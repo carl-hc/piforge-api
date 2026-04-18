59ddbc50473f49cf0cbbe5bc5f121e49fea16065
CarlHC <carlhc91@gmail.com>
2026-04-18 10:18:04 +0200

    add command resolver

---
+++ src/main/java/org/raspberry/piforge/engine/resolver/CommandResolver.java
@@ -0,0 +1,67 @@
+package org.raspberry.piforge.engine.resolver;
+
+import org.raspberry.piforge.core.exception.InternalServerErrorException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Component
+public class CommandResolver {
+
+    public List<String> resolve(String command, Map<String, String> params) {
+        List<String> result = new ArrayList<>();
+
+        Pattern pattern = Pattern.compile("\"([^\"]*)\"|(\\S+)");
+        Matcher matcher = pattern.matcher(command);
+
+        while (matcher.find()) {
+            String token = this.extractToken(matcher);
+            String value = this.resolveToken(token, params);
+
+            result.add(value);
+        }
+
+        return result;
+    }
+
+    private String extractToken(Matcher matcher) {
+        return matcher.group(1) != null
+                ? matcher.group(1)
+                : matcher.group(2);
+    }
+
+    private String resolveToken(String token, Map<String, String> params) {
+        StringBuilder result = new StringBuilder();
+
+        Pattern pattern = Pattern.compile("\\{\\{\\s*(.*?)\\s*\\}\\}");
+        Matcher matcher = pattern.matcher(token);
+
+        while (matcher.find()) {
+            String param = this.extractParam(matcher);
+            String value = this.resolveParam(param, params);
+
+            matcher.appendReplacement(result, Matcher.quoteReplacement(value));
+        }
+
+        matcher.appendTail(result);
+
+        return result.toString();
+    }
+
+    private String extractParam(Matcher matcher) {
+        return matcher.group(1);
+    }
+
+    private String resolveParam(String param, Map<String, String> params) {
+        if (!params.containsKey(param)) {
+            throw new InternalServerErrorException(String.format("Error resolving param %s", param));
+        }
+
+        return params.get(param);
+    }
+
+}
